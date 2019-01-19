bb502a4a47ec9b07b1b04079a297c7bb11c1a108
Noah Huppert <contact@noahh.io>
2019-01-19 11:06:19 -0500

    cp to boot: added opt to copy boot to repo

--- scripts/cp-to-boot.sh
+++ scripts/cp-to-boot.sh
@@ -4,7 +4,12 @@
 #
 # USAGE
 #
-#	cp-to-boot.sh
+#	cp-to-boot.sh [--reverse]
+#
+# OPTIONS
+#
+#	--reverse    Copy repository copy in boot partition to 
+#	             current directory
 #
 # BEHAVIOR
 #
@@ -14,20 +19,56 @@
 #	linux installations.
 #?
 
+# Exit on any error
 set -e
 
-# Check if root
+# {{{1 Check if root
 if [[ "$EUID" != "0" ]]; then
 	echo "Error: Script must run as root" >&2
 	exit 1
 fi
 
-# Copy
+# {{{1 Arguments
+while [ ! -z "$1" ]; do
+	case "$1" in 
+		--reverse)
+			reverse="true"
+			shift
+			;;
+
+		*)
+			echo "Error: Invalid argument \"$1\"" >&2
+			exit 1
+			;;
+	esac
+done
+
+# {{{1 Copy
+# {{{2 Resolve paths
 repo_dir=$(pwd -P)/$(dirname "$0")/..
+repo_dir=$(realpath "$repo_dir")
+
 boot_dir=/boot/linux-install
 
-if ! cp -r "$repo_dir" "$boot_dir"; then
-	echo "Error: Failed to copy $repo_dir to $boot_dir" >&2
+# {{{2 Determine copy direction
+if [ -z "$reverse" ]; then
+	# If NOT reversed
+	from="$repo_dir"
+	to="$boot_dir"
+else
+	# Reversed
+	if [ ! -d "$boot_dir" ]; then
+		echo "Error: --reverse option cannot be provided if $boot_dir does not exist" >&2 
+		exit 1
+	fi
+
+	from="$boot_dir"
+	to="$repo_dir"
+fi
+
+# {{{2 Actually copy
+if ! cp -r "$from" "$to"; then
+	echo "Error: Failed to copy $from to $to" >&2
 	exit 1
 fi
 
