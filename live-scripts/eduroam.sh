b3b4a7d7528b0de730558ef44ec543304a867649
Noah Huppert <contact@noahh.io>
2019-03-03 03:52:58 -0500

    live scripts: eduroam: wrote script to connect to eduroam

---
+++ live-scripts/eduroam.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+#?
+# eduroam.sh - Configures a connection to eduroam
+#
+# USAGE
+#
+# 	eduroam.sh OPTIONS
+#
+# OPTIONS
+#
+#	-u USERNAME    Eduroam username including `@school.edu`
+#	-p PASSWORD    Password
+#	-h             Show help text
+#
+# BEHAVIOR
+#
+#	Configures wpa_supplicant to connect to eduroam.#
+#
+#?
+
+# {{{1 Configuration
+ssid="eduroam"
+wpa_config="/etc/wpa_supplicant/config.conf"
+
+# {{{1 Options
+# {{{2 Get
+while getopts "u:p:h" opt; do
+	case "$opt" in 
+		u)
+			user="$OPTARG"
+			;;
+
+		p)
+			password="$OPTARG"
+			;;
+
+		h)
+			show-help "$0"
+			exit 1
+			;;
+
+		'?')
+			echo "Error: Unknown option \"$opt\"" >&2
+			exit 1
+			;;
+	esac
+done
+
+# {{{2 Verify
+# {{{3 user
+if [ -z "$user" ]; then
+	echo "Error: -u USER option required" >&2
+	exit 1
+fi
+
+if [[ ! "$user" =~ .*@.* ]]; then
+	echo "Error: -u USER option must be in format user@host.tld" >&2
+	exit 1
+fi
+
+# {{{3 password
+if [ -z "$password" ]; then
+	echo "Error: -p PASSWORD option required" >&2
+	exit 1
+fi
+
+# {{{1 Check if already configured
+if cat "$wpa_config" | grep "ssid=\"$ssid\"" &> /dev/null; then
+	echo "Error: Entry for $ssid already in $wpa_config" >&2
+	exit 1
+fi
+
+# {{{1 Generate passphrase portion of config
+psk=$(wpa_passphrase "$ssid" "$password" | grep psk)
+
+if [[ "$?" != "0" ]]; then
+	echo "$psk"
+	echo "Error: Failed to generate passphrase portion of configuration" >&2
+	exit 1
+fi
+
+# {{{1 Write configuration
+config="network={\n"
+config+="        ssid=\"$ssid\"\n"
+config+="        key_mgmt=WPA-EAP\n"
+config+="        eap=TTLS\n"
+config+="        phase2=\"auth=PAP\"\n"
+config+="        identity=\"$user\"\n"
+config+="$psk\n"
+config+="}"
+
+echo "Configuration:"
+echo "$config"
+echo
+
+if ! echo -e "$config" >> "$wpa_config"; then
+	echo "Error: Failed to write configuration to $wpa_config" >&2
+	exit 1
+fi
+
+if ! sv restart dhcpcd; then
+	echo "Error: Failed to restart dhcpcd service" >&2
+	exit 1
+if
+
+echo "DONE"
