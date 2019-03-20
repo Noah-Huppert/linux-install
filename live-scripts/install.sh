380f6c87d63039a5e931aca4382828b4d0ca86b1
Noah Huppert <contact@noahh.io>
2019-03-20 15:48:20 -0400

    live scripts: install: made use correct mirror url

--- live-scripts/install.sh
+++ live-scripts/install.sh
@@ -130,7 +130,7 @@ echo "# Installing Void Linux #"
 echo "#########################"
 
 if ! xbps-install -Sy \
-	-R http://auto.voidlinux.org/current \
+	-R https://alpha.de.repo.voidlinux.org/current/x86_64-repodata \
 	-r /mnt \
 	base-system lvm2 cryptsetup refind; then
 
