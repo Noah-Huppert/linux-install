9e402cf0055dc101b9f65b563ad5197e195bb08a
Noah Huppert <contact@noahh.io>
2019-03-20 23:25:23 -0400

    setup scripts: mk iso: cleaned up, made include useful tools in ISO

--- setup-scripts/mk-iso.sh
+++ setup-scripts/mk-iso.sh
@@ -10,6 +10,7 @@
 #
 #	-o ISO_OUT    Location to write ISO
 #	-a ARCH       Architecture to build ISO of
+#	-f            Force ISO to be rebuilt
 #	-h            Show help text
 #
 # BEHAVIOR
@@ -25,37 +26,42 @@
 set -e
 
 # {{{1 Configuration
+prog_dir=$(realpath $(dirname "$0"))
+
 mklive_dir="/opt/void-mklive"
+mklive_sh="$mklive_dir/mklive.sh"
+
+iso_pkgs="neovim cryptsetup curl unzip"
+iso_rootfs_repo_dir="/opt/linux-install"
 
 arch_x86_64_glibc="x86_64"
 arch_x86_64_musl="x86_64-musl"
 
+# {{{1 Helpers
+function die() {
+	echo "Error: $@" >&2
+	exit 1
+}
+
 # {{{1 Check if running on Void
 if ! lsb_release -c | grep 'void' &> /dev/null; then
-	echo "$0 can only be run on Void Linux due to compatibility issues with XBPS tools" >&2
-	exit 1
+	die "$0 can only be run on Void Linux due to compatibility issues with XBPS tools"
 fi
 
 # {{{1 Options
 # {{{2 Get
 while getopts "o:a:h" opt; do
 	case "$opt" in
-		o)
-			iso_out="$OPTARG"
-			;;
-
-		a)
-			arch="$OPTARG"
-			;;
-
+		o) iso_out="$OPTARG"  ;;
+		a) arch="$OPTARG"  ;;
+		f) force_iso="true" ;;
 		h)
 			echo "$0 -o ISO_OUT -a ARCH [-h]"
 			exit 1
 			;;
 
 		'?')
-			echo "Error: Unknown option \"$opt\"" >&2
-			exit 1
+			die "Unknown option \"$opt\""
 			;;
 	esac
 done
@@ -63,134 +69,115 @@ done
 # {{{2 Verify
 # {{{3 iso_out
 if [ -z "$iso_out" ]; then
-	echo "Error: -o ISO_OUT option required" >&2
-	exit 1
+	die "-o ISO_OUT option required"
 fi
 
 # {{{3 arch
 if [ -z "$arch" ]; then
-	echo "Error: -a ARCH option required" >&2
-	exit 1
+	die "-a ARCH option required"
 fi
 
 if [[ "$arch" != "$arch_x86_64_glibc" && "$arch" != "$arch_x86_64_musl" ]]; then
-	echo "Error: -a ARCH must be one of: $arch_x86_64_glibc, $arch_x86_64_musl" >&2
-	exit 1
+	die "-a ARCH must be one of: $arch_x86_64_glibc, $arch_x86_64_musl"
 fi
 
-# {{{1 Check for required software
-echo "#############################"
-echo "# Checking For Dependencies #"
-echo "#############################"
+# {{{1 Check if ISO has already been built
+if [ -f "$iso_out" ]; then
+	if [ -z "$force_iso" ]; then
+		die "$iso_out already exists"
+	else
+		if ! rm "$iso_out"; then
+			die "Failed to delete old ISO file: $iso_out"
+		fi
+	fi
+fi
 
+# {{{1 Check for dependencies
+pkgs_to_install=()
+
+# {{{2 Software
 for prog in curl tar unzip make xz git; do
-	# Check if program is in the path
 	if ! which "$prog" &> /dev/null; then
-		# Install
-		echo "$prog not installed, installing"
-
-		if ! xbps-install -Sy "$prog"; then
-			echo "Error: Failed to install $pro" >&2
-			exit 1
-		fi
-
-		# Special install steps
-		case "$prog" in
-			git)
-				git config --global user.name "root"
-				git config --global user.email "foo@bar.com"
-				;;
-		esac
-
-		# Final check
-		if ! which "$prog" &> /dev/null; then
-			echo "Error: Installed $prog but was not in PATH" >&2
-			exit 1
-		fi
+		echo "will install $prog"
+		pkgs_to_install+=("$prog")
 	fi
 done
 
-# {{{1 Check for required libraries
-for lib in liblz4.so.1 libreadline.so.8; do
-	# Check if library file exists
+# {{{2 Libraries
+for lib in liblz4 libreadline; do
 	if ! ls /usr/lib | grep "$lib" &> /dev/null; then
-		# Determine package to install
 		case "$lib" in
-			liblz4.so.1)
-				pkg_name="liblz4"
-				;;
+			liblz4) pkg_name="liblz4"  ;;
 
-			libreadline.so.8)
-				pkg_name="readline-devel"
-				;;
+			libreadline) pkg_name="readline-devel"  ;;
 		esac
 
-		# Install
-		if ! xbps-install -Sy "$pkg_name"; then
-			echo "Error: Failed to install $pkg_name to fullfil $lib" >&2
-			exit 1
-		fi
-
-		# Final check
-		if ! ls /usr/lib | grep "$lib"; then
-			echo "Error: Installed $pkg_name to fullful $lib but library was not found" >&2
-			exit 1
-		fi
+		echo "$lib library not found, will install $pkg_name"
+		pkgs_to_install+=("$prog")
 	fi
 done
 
-# {{{1 Install void-linux/void-mklive
-echo "#####################################"
-echo "# Installing void-linux/void-mklive #"
-echo "#####################################"
-
-# {{{2 Download 
-if [ ! -d "$mklive_dir" ]; then
-	if ! git clone "https://github.com/void-linux/void-mklive.git" "$mklive_dir"; then
-		echo "Error: Failed to download void-linux/void-mklive" >&2
-		exit 1
+# {{{2 Install packages if required
+if [ ! -z "$pkgs_to_install" ]; then
+	if ! xbps-install -Sy "${pkgs_to_install[@]}"; then
+		die "Failed to install dependencies: ${pkgs_to_install[@]}"
 	fi
 fi
 
-# {{{2 Build
-mklive_sh="$mklive_dir/mklive.sh"
-
-if [ ! -f "$mklive_sh" ]; then
-	if ! make -C "$mklive_dir"; then
-		echo "Error: Failed to build void-linux/void-mklive" >&2
-		exit 1
+# {{{2 void-mklive
+if [ ! -d "$mklive_dir" ]; then
+	# {{{3 Download
+	if ! git clone "https://github.com/void-linux/void-mklive.git" "$mklive_dir"; then
+		die "Failed to download void-mklive"
 	fi
-else
-	echo "Already built"
+
+	# {{{3 Build
+	if ! make -C "$mklive_dir"; then
+		die "Failed to build void-linux/void-mklive"
+	fi
 fi
 
 # {{{1 Make Void ISO
-echo "###################"
-echo "# Build Void ISO #"
-echo "###################"
+# {{{2 Check running as sudoer
+echo "Running $mklive_sh as sudo, may prompt for your sudo password"
 
-# {{{2 If iso doesn't exist, make
-if [ ! -f "$iso_out" ]; then
-	# {{{3 Check running as sudoer
-	echo "Running $mklive_sh as sudo, may prompt for your sudo password"
+if [[ "$EUID" != "0" ]]; then
+	mklive_run_args="sudo"
+fi
 
-	if [[ "$EUID" != "0" ]]; then
-		mklive_run_args="sudo"
+# {{{2 Construct ISO rootfs
+iso_rootfs_dir="/var/tmp/void-mklive-tmp-iso-rootfs"
+
+# {{{3 Make cleanup ISO rootfs directory on exit
+function iso_rootfs_cleanup() {
+	if [ -d "$iso_rootfs_dir" ]; then
+		if ! rm -rf "$iso_rootfs_dir"; then
+			die "Failed to delete ISO root file system directory: $iso_rootfs_dir"
+		fi
 	fi
+}
 
-	# {{{3 Run commands in void-mklive dir
-	cd "$mklive_dir"
+trap iso_rootfs_cleanup EXIT
 
-	# {{{3 Make ISO
-	if ! $mklive_run_args \
-		"$mklive_sh" \
-		-o "$iso_out" \
-		-a "$arch"; then
-		echo "Error: Failed to build Void Linux ISO" >&2
-		exit 1
-	fi
-else
-	echo "Already built"
+# {{{3 Make ISO rootfs directory
+if ! mkdir -p "$iso_rootfs_dir"; then
+	die "Failed to create ISO root file system directory: $iso_rootfs_dir"
+fi
+
+# {{{3 Copy this repository into ISO rootfs
+if ! cp -R "$prog_dir" "$iso_rootfs_repo_dir"; then
+	die "Failed to copy repository into ISO root file system"
+fi
+
+# {{{2 Make ISO
+if ! cd "$mklive_dir" && $mklive_run_args \
+	"$mklive_sh" \
+	-o "$iso_out" \
+	-p "$iso_pkgs" \
+	-I "$iso_rootfs_dir" \
+	-a "$arch"; then
+
+	die "Failed to build Void Linux ISO"
 fi
 
 echo "DONE"
