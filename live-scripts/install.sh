27c3d31b3952689ab6e65d7ade5ec80e8f23e5dc
Noah Huppert <contact@noahh.io>
2019-03-30 03:12:32 -0400

    live scripts: install: made use die(), made copy setup script to /mnt

--- live-scripts/install.sh
+++ live-scripts/install.sh
@@ -32,6 +32,12 @@ set -e
 # {{{1 Configuration
 prog_dir=$(realpath $(dirname "$0")) 
 
+# {{{1 Helpers
+function die() {
+	echo "Error: $@" >&2
+	exit 1
+}
+
 # {{{1 Options
 # {{{2 Get
 while getopts "c:b:" opt; do
@@ -39,8 +45,7 @@ while getopts "c:b:" opt; do
 		c) container="$OPTARG" ;;
 		b) boot_partition="$OPTARG" ;;
 		'?')
-			echo "Error: Unknown option \"$opt\"" >&2
-			exit 1
+			die "Unknown option \"$opt\""
 			;;
 	esac
 done
@@ -48,24 +53,20 @@ done
 # {{{2 Verify
 # {{{3 CONTAINER_NAME
 if [ -z "$container" ]; then
-	echo "Error: -c CONTAINER_NAME option required" >&2
-	exit 1
+	die "-c CONTAINER_NAME option required"
 fi
 
 if [ ! -e "/dev/mapper/$container" ]; then
-	echo "Error: -c CONTAINER_NAME does not exist in /dev/mapper" >&2
-	exit 1
+	die "-c CONTAINER_NAME does not exist in /dev/mapper"
 fi
 
 # {{{3 BOOT_PARTITION
 if [ -z "$boot_partition" ]; then
-	echo "Error: -b BOOT_PARTITION option required" >&2
-	exit 1
+	die "-b BOOT_PARTITION option required"
 fi
 
 if [ ! -e "$boot_partition" ]; then
-	echo "Error: -b BOOT_PARTITION does not exist" >&2
-	exit 1
+	die "-b BOOT_PARTITION does not exist"
 fi
 
 # {{{1 Mount devices in preparation for setup
@@ -77,9 +78,7 @@ function mount_cleanup() {
 	# Unmount in reverse order
 	mnts=("/mnt/dev" "/mnt/proc" "/mnt/sys" "/mnt/run" "/mnt/boot/efi" "/mnt")
 	if ! umount -R /mnt; then
-		echo "Error: Failed to umount /mnt" >&2
-		echo "Ensure ${mnts[@]} are unmounted manually" >&2
-		exit 1
+		die "Failed to umount /mnt. Unmount ${mnts[@]} manually"
 	fi
 }
 
@@ -87,33 +86,28 @@ trap mount_cleanup EXIT
 
 # {{{2 Root file system
 if ! mount "/dev/mapper/$container" /mnt; then
-	echo "Error: Failed to mount /dev/mapper/$container in /mnt" >&2
-	exit 1
+	die "Failed to mount /dev/mapper/$container in /mnt"
 fi
 
 # {{{2 Boot partition
 if ! mkdir -p /mnt/boot/efi; then
-	echo "Error: Failed to make mount point /mnt/boot/efi" >&2
-	exit 1
+	die "Failed to make mount point /mnt/boot/efi"
 fi
 
 if ! mount "$boot_partition" /mnt/boot/efi; then
-	echo "Error: Failed to mount $boot_partition in /mnt/boot/efi" >&2
-	exit 1
+	die "Failed to mount $boot_partition in /mnt/boot/efi"
 fi
 
 # {{{2 Mount system directories
 for dir in dev proc sys run; do
 	# {{{3 Create mount point
 	if ! mkdir -p "/mnt/$dir"; then
-		echo "Error: Failed to create mount point /mnt/$dir" >&2
-		exit 1
+		die "Failed to create mount point /mnt/$dir"
 	fi
 
 	# {{{3 Create recursive bind mount
 	if ! mount --rbind "/$dir" "/mnt/$dir"; then
-		echo "Error: Failed to create a recursive bind mount for /$dir in /mnt/$dir" >&2
-		exit 1
+		die "Failed to create a recursive bind mount for /$dir in /mnt/$dir"
 	fi
 done
 
@@ -125,24 +119,20 @@ echo "#########################"
 # {{{2 Copy repository signing keys
 # {{{3 Configuration files
 if ! mkdir -p /mnt/usr/share; then
-	echo "Error: Failed to create /mnt/usr/share directory" >&2
-	exit 1
+	die "Failed to create /mnt/usr/share directory"
 fi
 
 if ! cp -a /usr/share/xbps.d /mnt/usr/share/; then
-	echo "Error: Failed to copy XBPS configuration files to new system" >&2
-	exit 1
+	die "Failed to copy XBPS configuration files to new system"
 fi
 
 # {{{3 Keys
 if ! mkdir -p /mnt/var/db/xbps/keys; then
-	echo "Error: Failed to create XBPS keys directory" >&2
-	exit 1
+	die "Failed to create XBPS keys directory"
 fi
 
 if ! cp /var/db/xbps/keys/*.plist /mnt/var/db/xbps/keys; then
-	echo "Error: Failed to copy XBPS keys" >&2
-	exit 1
+	die "Failed to copy XBPS keys"
 fi
 
 # {{{2 Install packages
@@ -151,14 +141,12 @@ if ! xbps-install -Sy \
 	-r /mnt \
 	base-system lvm2 salt; then
 
-	echo "Error: Failed to install Void Linux" >&2
+	die "Failed to install Void Linux"
 	mount_cleanup
-	exit 1
 fi
 
 if ! sync; then
-	echo "Error: Failed to sync file system" >&2
-	exit 1
+	die "Failed to sync file system"
 fi
 
 # {{{1 Run setup
@@ -166,9 +154,19 @@ echo "#######################################"
 echo "# Running Setup Script In /mnt Chroot #"
 echo "#######################################"
 
-if ! xbps-uchroot /mnt "$prog_dir/setup.sh"; then
-	echo "Error: Failed to run setup script" >&2
-	exit 1
+chroot_setup_script_path="/tmp/setup.sh"
+mnt_setup_script_path="/mnt$chroot_setup_script_path"
+
+if ! cp "$prog_dir/setup.sh" "$mnt_setup_script_path"; then
+	die "Failed to copy setup script to mount directory"
+fi
+
+if ! xbps-uchroot /mnt "$chroot_setup_script_path"; then
+	die "Failed to run setup script"
+fi
+
+if ! rm "$mnt_setup_script_path"; then
+	die "Failed to remove copy of setup script in mount directory"
 fi
 
 # {{{1 Cleanup
@@ -178,9 +176,7 @@ echo "###########"
 
 # {{{2 Close cryptsetup container
 if ! cryptsetup close "$container"; then
-	echo "Error: Failed to close DM-Crypt LUKS container \"$container\"" >&2
-	echo "You will have to manually run \"cryptsetup close $container\"" >&2
-	exit 1
+	die "Failed to close DM-Crypt LUKS container \"$container\". Run \"cryptsetup close $container\""
 fi
 
 # {{{1 Done
