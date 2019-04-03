a935ba30938a742debb643fa31c9def9b25f4900
Noah Huppert <contact@noahh.io>
2019-04-03 07:04:07 -0400

    salt: users: zsh profile: user bin: made add /home/noah/bin to path

---
+++ salt/users/zprofile.d/0120_user_bin.zsh
@@ -0,0 +1,8 @@
+# Add directories in $HOME/bin to the PATH
+
+home_bin="$HOME/bin"
+
+# Add all top level directories in bin
+while read -r d; do
+    PATH="$PATH:$d"
+done <<< $(find "$home_bin" -maxdepth 1 ! -path . -type d)
