4e14331c98b4c28966fbea3560eec5dd92c19097
Noah Huppert <contact@noahh.io>
2019-04-04 11:31:57 -0400

    salt: emacs: made backup files in dedicated directory

--- salt/emacs/init.el
+++ salt/emacs/init.el
@@ -1,7 +1,21 @@
+; Initialize package manager
 (require 'package)
+
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+
 (setq package-enable-at-startup nil)
 (package-initialize) 
 
+; Enable VIM keybindings
 (require 'evil)
 (evil-mode t)
+
+; Save all backup files in a dedicated directory
+; https://stackoverflow.com/a/2680682
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+  backup-by-copying t    ; Don't delink hardlinks
+  version-control t      ; Use version numbers on backups
+  delete-old-versions t  ; Automatically delete excess backups
+  kept-new-versions 20   ; how many of the newest versions to keep
+  kept-old-versions 5    ; and how many of the old
+)
