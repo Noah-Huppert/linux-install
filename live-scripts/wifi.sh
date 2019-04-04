e1658010b802e8f70b112a772be34a22a5564925
Noah Huppert <contact@noahh.io>
2019-04-04 00:58:52 -0400

    live scripts: wifi: made -p PASSWORD option optional

--- live-scripts/wifi.sh
+++ live-scripts/wifi.sh
@@ -9,12 +9,10 @@
 # OPTIONS
 #
 # 	-s SSID        Name of WiFi network
-# 	-p PASSWORD    Password of WiFi network
+# 	-p PASSWORD    (Optional) Password of WiFi network
 #	-h             Show help text
 #
 #?
-
-set -e
 
 # {{{1 Config
 wpa_supplicant_config_path="/etc/wpa_supplicant/wpa_supplicant.conf"
@@ -32,7 +30,7 @@ while getopts "s:p:h" opt; do
 		s) ssid="$OPTARG"  ;;
 		p) password="$OPTARG"  ;;
 		h)
-			echo "$0 -s SSID -p PASSWORD [-h]"
+			echo "$0 -s SSID [-p PASSWORD,-h]"
 			exit 1
 		'?')
 			die "Unknown option \"$opt\""
@@ -45,18 +43,27 @@ if [ -z "$ssid" ]; then
 	die "-s SSID option required"
 fi
 
-# {{{3 password
-if [ -z "$password" ]; then
-	die "-p PASSWORD option required"
-fi
-
 # {{{1 Make WPA supplicant entry
 if cat "$wpa_supplicant_config_path" | grep "ssid=\"$ssid\""; then
 	die "Entry for $ssid already exists in $wpa_supplicant_config_path"
 fi
 
-if ! wpa_passphrase "$ssid" "$password" >> "$wpa_supplicant_config_path"; then
+if [ -n "$password" ]; then
+    # {{{2 If password provided
+    if ! wpa_passphrase "$ssid" "$password" >> "$wpa_supplicant_config_path"; then
+	    die "Failed to add entry to WPA supplicant config file"
+    fi
+else
+    # {{{2 If no password provided
+    cat <<EOF >> "$wpa_supplicant_config_path"
+network={
+	ssid="$ssid"
+	key_mgmt=NONE
+}
+EOF
+    if [[ "$?" != "0" ]]; then
 	die "Failed to add entry to WPA supplicant config file"
+    fi
 fi
 
 # {{{1 Enable wpa_supplicant
