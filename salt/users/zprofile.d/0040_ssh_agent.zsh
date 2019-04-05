bcccbce24a72a8b9366720f5c40eae6d3ec476b3
Noah Huppert <contact@noahh.io>
2019-04-04 20:50:59 -0400

    salt: users: zsh unit: ssh agent: simplified and fixed

--- salt/users/zprofile.d/0040_ssh_agent.zsh
+++ salt/users/zprofile.d/0040_ssh_agent.zsh
@@ -1,17 +1,5 @@
-# Start SSH agent if one hasn't been started, or load configuration about 
-# already running agent
+# Start SSH agent if login shell
 
-# Location of file which will have information about a currently running 
-# ssh agent
-ssh_agent_info_f="$XDG_CONFIG_HOME/running-ssh-agent-info"
-
-# Check to see if ssh-agent is running for user
-if ! pgrep -u "$USER" ssh-agent &> /dev/null; then
-	# If not running, run and save info to file
-	ssh-agent > "$ssh_agent_info_f"
-fi
-
-# If environment doesn't contain ssh-agent information
-if [ -z "$SSH_AGENT_PID" ]; then
-	. "$ssh_agent_info_f" > /dev/null
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    eval "$(ssh-agent)"
 fi
