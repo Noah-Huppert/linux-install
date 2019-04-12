ba5273c3721ad7cd6312f929a6e65c9bf5818083
Noah Huppert <contact@noahh.io>
2019-04-12 16:07:55 -0400

    salt: zsh-profile: prompt: made collapse certian common paths

--- salt/zsh-profile/zprofile.d/0070_prompt.zsh
+++ salt/zsh-profile/zprofile.d/0070_prompt.zsh
@@ -16,12 +16,32 @@ function git_prompt() {
 	echo " %{$fg[green]%}git%{$reset_color%}:%{$fg[magenta]%}$branch%{$reset_color%}"
 }
 
+## Escapes paths for use in sed
+function escape_path() { # ( PATH )
+    echo "$1" | sed 's/\//\\\//g'
+}
+
+## Replaces pieces of path with shortcut name
+function shortcut_path() { # STDIN, ( FIND, REPLACE )
+    sed_str="s/$(escape_path $1)/$(escape_path $2)/g"
+    cat - | sed "$sed_str"
+}
+
+## Shows PWD with shortcuts to make shorter
+function pwd_prompt() {
+    d="$PWD"
+    d=$(echo "$d" | shortcut_path "$HOME/documents/school" "~/[school]")
+    d=$(echo "$d" | shortcut_path "$HOME/documents" "~/[docs]")
+    d=$(echo "$d" | shortcut_path "$HOME" "~")
+
+    echo "$d"
+}
+
 # Sets prompt variable
 function build_prompt() {
 	# HOSTNAME PATH git:BRANCH %#
-	export PROMPT="%{$fg[red]%}%~%{$reset_color%}$(git_prompt) %# "
+	export PROMPT="%{$fg[red]%}$(pwd_prompt)%{$reset_color%}$(git_prompt) %# "
 }
 
 build_prompt
-#add-zsh-hook chpwd build_prompt
 add-zsh-hook precmd build_prompt
