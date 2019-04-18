cdb40a349422b473d912f32d922c926aaaff3702
Noah Huppert <contact@noahh.io>
2019-04-18 11:20:12 -0400

    salt: zsh-profile: screenshot dir: set screenshot dir for snip tool

---
+++ salt/zsh-profile/zprofile.d/0011_screenshot_dir.zsh
@@ -0,0 +1,3 @@
+# Configure default screenshots directory for snip command
+# Command is from the https://github.com/Noah-Huppert/scripts repository
+export SNIP_DEFAULT_OUT="$HOME/pictures/screenshots"
