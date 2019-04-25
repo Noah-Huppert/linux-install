0185e1b60ef30103886e5979ee14ea30fce2d1ca
Noah Huppert <contact@noahh.io>
2019-04-25 01:21:08 -0400

    salt: polybar: configured to launch on both screens

--- salt/polybar/launch.sh
+++ salt/polybar/launch.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 0.01; done
 
 # Launch primary bar
-MONITOR=eDP-1 polybar primary -r &
+echo "Launching bar on eDP-1"
 
-echo "Bar launched..."
+MONITOR=eDP-1 polybar primary -r & disown 
+
+if ! xrandr -q | grep "HDMI1 connected (" &> /dev/null; then
+    echo "Launching br on HDMI1"
+    
+    MONITOR=HDMI1 polybar primary -r & disown
+fi
+
