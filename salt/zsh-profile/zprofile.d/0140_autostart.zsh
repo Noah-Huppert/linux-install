2e951446b1c24bd52698673bc2262ee8b808779f
Noah Huppert <contact@noahh.io>
2019-05-11 23:19:06 -0400

    salt: zsh-profile: autostart: made wireguard start async

--- salt/zsh-profile/zprofile.d/0140_autostart.zsh
+++ salt/zsh-profile/zprofile.d/0140_autostart.zsh
@@ -1,18 +1,22 @@
 # Automatically run commands on first start
 
-# Only run once
-if [ -z "$ZSH_UNIT_AUTOSTART_RUN" ]; then
+function __wg_start() {
     # Start wireguard
     if ! ip link show | grep {{ pillar.wireguard.interface }} &> /dev/null; then
 	unit-echo "Starting Wireguard"
 
 	if ! wg-quick up {{ pillar.wireguard.interface }}; then
-	    return $(unit-die "Failed to start Wireguard")
+	    unit-die "Failed to start Wireguard"
 	fi
     else
 	unit-echo "Wireguard already started"
     fi
+}
 
+# Only run once
+if [ -z "$ZSH_UNIT_AUTOSTART_RUN" ]; then   
     # Set flag so we know we've run
     export ZSH_UNIT_AUTOSTART_RUN="true"
+
+    __wg_start &
 fi
