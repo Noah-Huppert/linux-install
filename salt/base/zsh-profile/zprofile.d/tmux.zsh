41f66a1ddeabce4f4151563f246239d2bb9e1f55
Noah Huppert <contact@noahh.io>
2020-06-09 11:01:47 -0400

    Revert "zsh-profile: tmux: stopped recursive sourcing of config file"
    This reverts commit ba3c137a80e8d8d6beaf083fc512b48be149f0bd.

--- salt/base/zsh-profile/zprofile.d/tmux.zsh
+++ salt/base/zsh-profile/zprofile.d/tmux.zsh
@@ -2,4 +2,7 @@
 if [ -z "$TMUX" ] && [ -n "$DISPLAY" ]; then
 	# Start TMux and exit when TMux exits
 	exec tmux
+elif [ -n "$TMUX" ]; then # If TMux is already running
+	# Source configuration file
+	tmux source-file "{{ pillar.tmux.configuration_file }}"
 fi
