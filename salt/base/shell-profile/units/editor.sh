3b8e9bb809a6c053b21360e3f6c93e4bd5521a52
Noah Huppert <contact@noahh.io>
2020-11-26 02:05:33 -0500

    shell-profile: editor: changed EDITOR to use emacsclient w existing frame Now invokes emacsclient which conencts to the already running emacs client (in which ansi-mode is running and probably the caller of $EDITOR) so that a new buffer is simply opened. This allows the normal flow of editing files by invoking your editor in the command line. As before it would open a new whole new window which sucks.

--- salt/base/shell-profile/units/editor.sh
+++ salt/base/shell-profile/units/editor.sh
@@ -1,2 +1,5 @@
 # Set EDITOR so programs know which editor to open files in
-export EDITOR=emacs
+
+# Open emacs client in existing Emacs window, since this should be running in Emacs
+# in ansi-term by the inferior shell bash.
+export EDITOR="emacsclient --alternate-editor=''"
