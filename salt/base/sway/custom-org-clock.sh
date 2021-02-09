e4b821a0801a2d6918fd30647f119bc21e2ee128
Noah Huppert <contact@noahh.io>
2021-02-09 15:48:11 -0500

    sway: waybar: org-clock: fixed err handling, better org clock running check

--- salt/base/sway/custom-org-clock.sh
+++ salt/base/sway/custom-org-clock.sh
@@ -4,19 +4,24 @@
 # exits silently. The output is JSON formatted
 # for waybar.
 
+prog_dir=$(dirname $(realpath "$0"))
+err_file="$prog_dir/custom-org-clock.err"
+echo "" > "$err_file"
+
 resp_text=""
 resp_alt=clock
 resp_tooltip=""
 
 update_org_clock() {
-    clock_str=$(emacsclient --eval "(if org-clock-current-task (substring-no-properties (org-clock-get-clock-string)))" | sed 's/"//g')
-    
-    if [ "$?" -ne "0" ]; then
+        clock_str=$(emacsclient --eval "(if (boundp 'org-clock-current-task) (substring-no-properties (org-clock-get-clock-string)))" 2> "$err_file")
+    if [[ "$?" != "0" ]]; then
 	   resp_text="org clock error"
-	   resp_tooltip="$clock_str"
+	   resp_tooltip=$(cat "$err_file")
 	   return
     fi
 
+    clock_str=$(echo "$clock_str" | sed 's/"//g')
+    
     if [[ "$clock_str" == "nil" ]]; then
 	   # No clock running
 	   resp_alt=nil
