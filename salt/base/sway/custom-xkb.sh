41cfc7b4b7ba40b434a9de02eb5622ee4b86bf52
Noah Huppert <contact@noahh.io>
2022-02-17 15:32:55 -0500

    sway: disabled waybar xkb module
    This module was meant to help switch between languages. I fixed the
    path to the script (which was causing errors), but then the script was
    having errors itself. Since I don't use this module right now I
    disabled it. This fixed waybar from crashing on startup.

--- salt/base/sway/custom-xkb.sh
+++ salt/base/sway/custom-xkb.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 # Print current layout
-xkb-switch -p
+xkb_error() {
+    echo "{ \"text\": \"XKB error\" }"
+    exit 1
+}
+
+layout=$(xkb-switch -p) || xkb_error
+
+echo "{ \"text\": \"$layout\" }"
